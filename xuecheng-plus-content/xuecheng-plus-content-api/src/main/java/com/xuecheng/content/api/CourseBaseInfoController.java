cb610f13cd20f3ddc9e64fd150e11c62f8452907
echowz <1007761282@qq.com>
2023-07-31 21:20:50 +0800

    导入swagger，完成接口测试。

--- xuecheng-plus-content/xuecheng-plus-content-api/src/main/java/com/xuecheng/content/api/CourseBaseInfoController.java
+++ xuecheng-plus-content/xuecheng-plus-content-api/src/main/java/com/xuecheng/content/api/CourseBaseInfoController.java
@@ -5,22 +5,24 @@ import com.xuecheng.base.model.PageResult;
 import com.xuecheng.content.model.dto.QueryCourseParamsDto;
 import com.xuecheng.content.model.po.CourseBase;
 
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.web.bind.annotation.*;
 
 
+@Api(value = "课程信息编辑接口",tags = "课程信息编辑接口")
 @RestController
 public class CourseBaseInfoController {
 
-    @RequestMapping("/course/list")
+    @ApiOperation("课程查询接口")
+    @PostMapping("/course/list")
     public PageResult<CourseBase> list(PageParams pageParams, @RequestBody QueryCourseParamsDto queryCourseParams){
 
         return null;
 
     }
 
-    @RequestMapping("/hi")
+    @GetMapping ("/hi")
     public String sayHi(){
         return "hi!";
     }

f2ccc554e01508ddc7d5d137b00173ab432abb5c
echowz <1007761282@qq.com>
2023-07-31 20:05:03 +0800

    创建内容管理的实体类、基础工程中的分页参数、接口层测试文件、相关配置文件等。
    创建内容管理的实体类、基础工程中的分页参数、接口层测试文件、相关配置文件等。2
    
    创建内容管理的实体类、基础工程中的分页参数、接口层测试文件、相关配置文件等。3

--- xuecheng-plus-content/xuecheng-plus-content-api/src/main/java/com/xuecheng/ContentApplication.java
+++ xuecheng-plus-content/xuecheng-plus-content-api/src/main/java/com/xuecheng/ContentApplication.java
@@ -4,10 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class XuechengPlusContentApiApplication {
-
+public class ContentApplication {
     public static void main(String[] args) {
-        SpringApplication.run(XuechengPlusContentApiApplication.class, args);
+        SpringApplication.run(ContentApplication.class, args);
     }
-
 }

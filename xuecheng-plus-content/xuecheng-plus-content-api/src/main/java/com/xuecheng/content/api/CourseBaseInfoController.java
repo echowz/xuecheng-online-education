e6154e1956d5499aa24082806d91bf148e7bb1bf
echowz <1007761282@qq.com>
2023-08-01 15:47:36 +0800

    创建本地时间配置类，设置全局json序列化时间格式

--- xuecheng-plus-content/xuecheng-plus-content-api/src/main/java/com/xuecheng/content/api/CourseBaseInfoController.java
+++ xuecheng-plus-content/xuecheng-plus-content-api/src/main/java/com/xuecheng/content/api/CourseBaseInfoController.java
@@ -9,6 +9,10 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
 
 @Api(value = "课程信息编辑接口",tags = "课程信息编辑接口")
 @RestController
@@ -16,10 +20,15 @@ public class CourseBaseInfoController {
 
     @ApiOperation("课程查询接口")
     @PostMapping("/course/list")
-    public PageResult<CourseBase> list(PageParams pageParams, @RequestBody QueryCourseParamsDto queryCourseParams){
+    public PageResult<CourseBase> list(PageParams pageParams, @RequestBody(required=false) QueryCourseParamsDto queryCourseParams){
 
-        return null;
-
+        CourseBase courseBase = new CourseBase();
+        courseBase.setName("测试名称");
+        courseBase.setCreateDate(LocalDateTime.now());
+        List<CourseBase> courseBases = new ArrayList<>();
+        courseBases.add(courseBase);
+        PageResult<CourseBase> pageResult = new PageResult<>(courseBases,10,1,10);
+        return pageResult;
     }
 
     @GetMapping ("/hi")

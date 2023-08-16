66667c9d5a6e3837e4c44011551c33011f5bad8e
echowz <1007761282@qq.com>
2023-08-16 16:15:09 +0800

    测试minio的分块上传和合并功能

--- xuecheng-plus-media/xuecheng-plus-media-service/src/test/java/com/xuecheng/media/MinioTest.java
+++ xuecheng-plus-media/xuecheng-plus-media-service/src/test/java/com/xuecheng/media/MinioTest.java
@@ -2,10 +2,9 @@ package com.xuecheng.media;
 
 import com.j256.simplemagic.ContentInfo;
 import com.j256.simplemagic.ContentInfoUtil;
-import io.minio.GetObjectArgs;
-import io.minio.MinioClient;
-import io.minio.RemoveObjectArgs;
-import io.minio.UploadObjectArgs;
+import io.minio.*;
+import io.minio.messages.DeleteError;
+import io.minio.messages.DeleteObject;
 import org.apache.commons.compress.utils.IOUtils;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.MediaType;
@@ -13,6 +12,9 @@ import org.springframework.http.MediaType;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.FilterInputStream;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class MinioTest {
 
@@ -72,6 +74,65 @@ public class MinioTest {
             e.printStackTrace();
             System.out.println("删除失败");
         }
+    }
+
+
+    /**
+     *关于断点续传中的 分块上传minio 和合并块的测试
+     */
+    @Test
+    public void uploadChunk(){
+        String chunkFolderPath = "d:/dev_folder/chunk/";
+        File chunkFolder = new File(chunkFolderPath);
+        //分块文件
+        File[] files = chunkFolder.listFiles();
+        //将分块文件上传至minio
+        for (int i = 0; i < files.length; i++) {
+            try {
+                UploadObjectArgs uploadObjectArgs = UploadObjectArgs.builder().bucket("testbucket").object("chunk/" + i).filename(files[i].getAbsolutePath()).build();
+                minioClient.uploadObject(uploadObjectArgs);
+                System.out.println("上传分块成功"+i);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+    }
+
+    //合并文件，要求分块文件最小5M
+    @Test
+    public void test_merge() throws Exception {
+        List<ComposeSource> sources = Stream.iterate(0, i -> ++i)
+                .limit(4)
+                .map(i -> ComposeSource.builder()
+                        .bucket("testbucket")
+                        .object("chunk/".concat(Integer.toString(i)))
+                        .build())
+                .collect(Collectors.toList());
+
+        ComposeObjectArgs composeObjectArgs = ComposeObjectArgs.builder().bucket("testbucket").object("merge01.mp4").sources(sources).build();
+        minioClient.composeObject(composeObjectArgs);
+
+    }
+    //清除分块文件
+    @Test
+    public void test_removeObjects(){
+        //合并分块完成将分块文件清除
+        List<DeleteObject> deleteObjects = Stream.iterate(0, i -> ++i)
+                .limit(4)
+                .map(i -> new DeleteObject("chunk/".concat(Integer.toString(i))))
+                .collect(Collectors.toList());
+
+        RemoveObjectsArgs removeObjectsArgs = RemoveObjectsArgs.builder().bucket("testbucket").objects(deleteObjects).build();
+        Iterable<Result<DeleteError>> results = minioClient.removeObjects(removeObjectsArgs);
+        results.forEach(r->{
+            DeleteError deleteError = null;
+            try {
+                deleteError = r.get();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        });
     }
 
 }
